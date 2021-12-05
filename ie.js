7587865ba57092b7766cb0a3523cd5051d221efd
Mark.exe <markverb4@gmail.com>
2021-12-05 20:27:12 +0200

    A

--- ie.js
+++
@@ -1,9 +0,0 @@
-isIeOpen();
-function isIeOpen() {
-    if (document.getElementById('ie-win').style.display = 'block') {
-       document.getElementById('ie-bar').style.display = 'block';
-    } else if (document.getElementById('ie-win').style.display = 'none') {
-        console.log('IE IS NOT OPEN')
-       document.getElementById('ie-bar').style.display = 'none';
-    }
-}
